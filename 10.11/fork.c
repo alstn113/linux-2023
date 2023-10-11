f8c0b6519cc7ab107c5d5b260bc5f3e3232b4b04
김민수 MINSOO KIM <alstn113@gmail.com>
2023-10-11 15:36:37 +0900

    feat: fork example

---
+++ 10.11/fork.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <errno.h>
+#include <sys/types.h>
+#include
+
+int main()
+{
+  pid_t pid;
+
+  pid = fork();
+
+  if (pid > 0)
+  {
+    printf("I am the parent of pid=%d!\n", pid);
+  }
+  else if (!pid)
+  {
+    printf("I am the child\n");
+  }
+  else if (pid == -1)
+  {
+    perror("fork");
+  }
+
+  return 0;
+}

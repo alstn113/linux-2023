1f7674d356470058b9d5ce00aa436d3c816bda2a
김민수 MINSOO KIM <alstn113@gmail.com>
2023-12-06 15:18:12 +0900

    feat: get, set affinity

---
+++ 12.06/sched_setaffinity.c
@@ -0,0 +1,24 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <sched.h>
+
+int main(void)
+{
+    cpu_set_t set;
+    int ret;
+    int i;
+    CPU_ZERO(&set);
+    CPU_SET(0, &set);
+    CPU_CLR(1, &set);
+    ret = sched_getaffinity(0, sizeof(cpu_set_t), &set);
+    if (ret == -1)
+        perror("sched_setaffinity");
+
+    for (i = 0; i < CPU_SETSIZE; i++)
+    {
+        int cpu;
+        cpu = CPU_ISSET(i, &set);
+        printf("CPU %d is %s\n", i, cpu ? "set" : "unset");
+    }
+}
